6d562b83c155355ec410045d9edf280e270a09c6
Luis Manuel Gomez <132625020+ManuelGL10@users.noreply.github.com>
2024-03-18 18:03:03 -0600

    Revert "Design - Avance de diseños"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Inicio from './views/Inicio';
-import Nosotros from './views/Nosotros'
-import Producto from './views/Producto';
-import Servicio from './views/Servicio';
-import Login from './views/Login';
-import { Route, Routes } from 'react-router-dom';
-import '@fontsource/hind-madurai'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Inicio/>}/>
-      <Route path='/nosotros' element={<Nosotros/>}/>
-      <Route path='/producto' element={<Producto/>}/>
-      <Route path='/servicio' element={<Servicio/>}/>
-      <Route path='/login' element={<Login/>}/>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
