745820be7bcd9690a90e5d25759b12dc0977b839
Edgar.eggy <edgar.alvarado.21s@utzmg.edu.mx>
2024-10-21 16:00:44 -0600

    Delete import font hind_miradai in App.js

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ import DashboardRegistro from './views/DashboardRegistro';
 import DashboardConfig from './views/DashboardConfig';
 import DasboardCultivo from './views/DasboardCultivo';
 import VerificationPage from './views/VerificationPage';
-import '@fontsource/hind-madurai'
 import ProfileUser from './views/ProfileUser';
-import Navbar_Top from './components/Navbar_Top';
 import ProductosVenta from './views/ProductosVenta';
 import UserHome from './views/UserHome';
 import UserDevice from './views/UserDevice';
